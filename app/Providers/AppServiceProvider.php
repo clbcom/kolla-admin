c8f5a429ffc2365a575126b3f198b245ade28d0b
Cristhian Lima Blanco <cristhianlimablanco@gmail.com>
2024-06-11 19:15:01 -0400

    add: agregado provider para almacenamiento en la nube con backblaze

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use Zaxbux\BackblazeB2\Client;
+use League\Flysystem\Filesystem;
+use PhpParser\Node\Stmt\TryCatch;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
+use Zaxbux\Flysystem\BackblazeB2Adapter;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Contracts\Foundation\Application;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +26,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Storage::extend('b2', function (Application $app, array $config) {
+            $cliente = new Client([$config['key_id'], $config['application_key']]);
+            $adapter    = new BackblazeB2Adapter($cliente, $config['bucket_id']);
+            $filesystem = new Filesystem($adapter);
+
+            return new FilesystemAdapter($filesystem, $adapter, $config);
+        });
     }
 }
